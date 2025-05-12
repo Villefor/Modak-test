e73efc2a8fa07a51ebe79c54d1a42d9b04384e9e
Dario <dariovillefor@gmail.com>
2025-05-12 03:09:57 -0300

    feat: controllers and category action done for now

---
+++ modak-sample-app/app/controllers/useCategoryController.ts
@@ -0,0 +1,15 @@
+// src/controllers/CategoryController.ts
+import * as CategoryActions from "@/app/actions/CategoryAction";
+import { Category } from "@/interfaces/categoryInterface";
+
+export async function fetchCategoryList(): Promise<{
+  data: Category[] | null;
+  error: Error | null;
+}> {
+  try {
+    const data = await CategoryActions.listCategories();
+    return { data, error: null };
+  } catch (error: any) {
+    return { data: null, error };
+  }
+}
